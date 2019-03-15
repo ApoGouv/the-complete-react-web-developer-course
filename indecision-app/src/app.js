64bf1c1cfbad95594d02a65fef14c9349e45469d
ApoGouv <apo.gouv@gmail.com>
2019-03-15 23:21:57 +0200

    Section 6 - Webpack - till end 58 - ES6 class properties

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,12 +1,5 @@
-// import './utils.js';
-import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-console.log('app.js is running!!!');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 81));
-
-console.log('isAdult: ', isAdult(17));
-console.log('canDrink: ', canDrink(31));
-console.log('isSenior: ', isSenior(70));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -16,5 +16,15 @@ module.exports = {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    mode: 'development'
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
 };
