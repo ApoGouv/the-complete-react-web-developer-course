8775bd95ec77de00c054d43d88704035b24ff781
ApoGouv <apo.gouv@gmail.com>
2019-03-16 20:11:49 +0200

    Section 8 - Styling React

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
         contentLabel="Selected Option"
         onRequestClose={props.handleClearSelectedOption}
         appElement={document.getElementById('app')}
+        closeTimeoutMS={300}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Okay!</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>Okay!</button>
     </Modal>
 );
 
